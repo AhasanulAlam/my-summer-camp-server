64ba47cea4828e34a1b46adc4e915f838d3316cc
AhasanulAlam <ahasanulalam@gmail.com>
2023-06-09 21:34:09 +0800

    Create JWT token API

--- index.js
+++ index.js
@@ -32,6 +32,15 @@ async function run() {
         const instructorsCollection = client.db("mySummerCampDB").collection("instructors");
         const cartCollection = client.db("mySummerCampDB").collection("carts");
 
+
+        // JWT Token API
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        });
+
+
         // Get User API
         app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
