426c46dccadcf2824ce04c3d11a83d3e086ae4d3
AhasanulAlam <ahasanulalam@gmail.com>
2023-06-09 23:08:55 +0800

    Create Check Admin role API

--- index.js
+++ index.js
@@ -76,6 +76,18 @@ async function run() {
             res.send(result);
         });
 
+        // Check admin role  API
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+        })
+
         // Update User Admin Role API
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
